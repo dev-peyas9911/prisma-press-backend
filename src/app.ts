51a942f03d7ca7543dee007de660dafdd2c7950d
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-07-03 15:31:24 +0600

    Modular pattern implement

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,25 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import cookieParser from "cookie-parser";
+import { userRoutes } from "./modules/user/user.route";
 
 const app: Application = express();
 
-app.use(cors({
-    origin : config.app_url,
-    credentials : true,
-}))
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
+
+app.use("/api/users", userRoutes);
 
 export default app;
