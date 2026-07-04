7294a961a24e79c5dccec832d239e4f202d64010
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-07-04 17:19:33 +0600

    Implementing jwt access and refresh token

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import config from "./config";
 import cookieParser from "cookie-parser";
 import { userRoutes } from "./modules/user/user.route";
+import { authRoutes } from "./modules/auth/auth.route";
 
 const app: Application = express();
 
@@ -22,5 +23,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/users", userRoutes);
+
+app.use("/api/auth", authRoutes);
 
 export default app;
