759b244e9f5b56d4208c2883876f9d783ec77929
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-07-05 15:28:54 +0600

    Create post and Get all post api created

---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,48 @@
+import { prisma } from "../../lib/prisma";
+import { ICreatePostPayload } from "./post.interface";
+
+const createPost = async (payload: ICreatePostPayload, userId: string) => {
+  const result = await prisma.post.create({
+    data: {
+      ...payload,
+      authorId: userId,
+    },
+  });
+
+  return result;
+};
+
+const getAllPosts = async () => {
+  const posts = await prisma.post.findMany({
+    include: {
+      author: {
+        omit: {
+          password: true,
+        },
+      },
+      comments: true,
+    },
+  });
+
+  return posts;
+};
+
+const getPostById = async () => {};
+
+const updatePost = async () => {};
+
+const deletePost = async () => {};
+
+const getPostsStats = async () => {};
+
+const getMyPosts = async () => {};
+
+export const postService = {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  getPostsStats,
+  getMyPosts,
+};
